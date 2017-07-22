62db344e033e8e5e2977c723d3364deb8da7c9e1
Harry Oslislo <oslislo1@hotmail.com.com>
2017-07-22 14:13:27 -0400

    Initial commit

---
+++ funtion2.py
@@ -0,0 +1,23 @@
+def addnums(x,y):
+    return x + y
+# change to push
+print addnums(2,3)
+print addnums(0x1f,3.3)
+# "0x" is a hexadecimal number
+# 9 09, 10 0a, 11 0b, ... 15 0f, 16 10, 31 1f ...
+print addnums("a","b")
+
+def addnums(x,y):
+    if isinstance(x,(float,int)) and isinstance(y,(float,int)):
+        return x + y
+    print("I cannot add these types (" + str(type(x)) + ", " + str(type(y)) + ")")
+    return
+
+print addnums(2,3.0)
+print addnums("cat",23232)
+
+# 5
+# 34.3
+# ab
+# I cannot add these types (<type 'str'>, <type 'int'>)
+# None
